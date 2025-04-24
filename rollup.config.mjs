e35cc9d4dad42d0a26c72dd1f22539fa87f7b0a9
amin23877 <amin23877@gmail.com>
2025-04-24 20:45:10 +0330

    feat: add CSS build process and update dependencies
    - Added a new script to build CSS using PostCSS.
    - Updated dependencies for Tailwind CSS and PostCSS packages.
    - Modified PostCSS configuration to use the correct plugin format.
    - Enhanced Rollup configuration to include CSS output in the build process.
    - Created a new style.css file to compile global styles from Tailwind.
    - Adjusted the import path for the Tailwind configuration in globals.css.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,27 @@
+/* eslint-disable import/no-anonymous-default-export */
 
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
   // JS + CSS bundle
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
@@ -28,7 +29,7 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json", declaration: false }),
+      typescript({ tsconfig: './tsconfig.json', declaration: false }),
       postcss({
         extract: true,
         minimize: true,
@@ -42,8 +43,18 @@ export default [
   },
   // Type declarations
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [dts()],
+  },
+  {
+    input: 'src/app/style.css',
+    output: [{ file: 'dist/index.css', format: 'es' }],
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: true,
+      }),
+    ],
   },
 ];
